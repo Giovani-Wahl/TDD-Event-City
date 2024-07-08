2f2091b48fb99b4511c692d502a7954190de4812
Giovani Wahl <giovaniwahl@gmail.com>
2024-07-08 11:58:49 -0300

    testes controller city ok

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -2,12 +2,14 @@ package com.devsuperior.demo.controllers;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,8 +26,9 @@ public class CityController {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll(pageable));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto){
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(dto));
     }
     @PutMapping("/{id}")
